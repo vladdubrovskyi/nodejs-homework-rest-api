ac81063082b2aa4c5f9621e94f780742ddcea909
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2023-01-15 17:43:11 +0200

    first commit

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,20 +1,15 @@
-const {
-  listContacts,
-  getContactById,
-  createContact,
-  deleteContact,
-  updateContact,
-} = require("../models/contacts");
 const { MyError } = require("../helpers/index");
+const { Contact } = require("../models/contacts");
 
 async function getContacts(_, res) {
-  const contacts = await listContacts();
+  const contacts = await Contact.find();
+  console.log(contacts);
   return res.json(contacts);
 }
 
 async function getContact(req, res, next) {
   const { contactId } = req.params;
-  const contact = await getContactById(contactId);
+  const contact = await Contact.findById(contactId);
   if (!contact) {
     return next(new MyError("Not Found", 404));
   }
@@ -23,23 +18,23 @@ async function getContact(req, res, next) {
 
 async function addContact(req, res) {
   const { name, email, phone } = req.body;
-  const newContact = await createContact(name, email, phone);
+  const newContact = await Contact.create({ name, email, phone });
   return res.status(201).json(newContact);
 }
 
 async function removeContact(req, res, next) {
   const { contactId } = req.params;
-  const contact = await getContactById(contactId);
+  const contact = await Contact.findById(contactId);
   if (!contact) {
     return next(new MyError("No Contact", 404));
   }
-  await deleteContact(contactId);
+  await Contact.findByIdAndRemove(contactId);
   return res.status(200).json(contact);
 }
 
 async function changeContact(req, res, next) {
   const { contactId } = req.params;
-  const contact = await updateContact(contactId, req.body);
+  const contact = await Contact.findByIdAndUpdate(contactId, req.body);
   if (!contact) {
     return next(new MyError("Not Found", 404));
   }
--- models/contacts.js
+++ models/contacts.js
@@ -1,69 +1,100 @@
-const fs = require("fs/promises");
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
+// const fs = require("fs/promises");
+// const path = require("path");
+// const { v4: uuidv4 } = require("uuid");
 
-const dataBasePath = path.resolve(__dirname, "contacts.json");
+// const dataBasePath = path.resolve(__dirname, "contacts.json");
 
-async function readDataBase() {
-  const dataBaseRaw = await fs.readFile(dataBasePath);
-  const dataBase = JSON.parse(dataBaseRaw);
-  return dataBase;
-}
+// async function readDataBase() {
+//   const dataBaseRaw = await fs.readFile(dataBasePath);
+//   const dataBase = JSON.parse(dataBaseRaw);
+//   return dataBase;
+// }
 
-async function writeDataBase(dataBase) {
-  await fs.writeFile(dataBasePath, JSON.stringify(dataBase, null, 2));
-}
+// async function writeDataBase(dataBase) {
+//   await fs.writeFile(dataBasePath, JSON.stringify(dataBase, null, 2));
+// }
 
-const listContacts = async () => {
-  const dataBase = await readDataBase();
-  return dataBase;
-};
+// const listContacts = async () => {
+//   const dataBase = await readDataBase();
+//   return dataBase;
+// };
 
-const getContactById = async (contactId) => {
-  const dataBase = await readDataBase();
-  const contact = dataBase.find((contact) => contact.id === contactId);
+// const getContactById = async (contactId) => {
+//   const dataBase = await readDataBase();
+//   const contact = dataBase.find((contact) => contact.id === contactId);
 
-  return contact || null;
-};
+//   return contact || null;
+// };
 
-const deleteContact = async (contactId) => {
-  const dataBase = await readDataBase();
-  const updatedDataBase = dataBase.filter(
-    (contact) => contact.id !== contactId
-  );
-  await writeDataBase(updatedDataBase);
-};
+// const deleteContact = async (contactId) => {
+//   const dataBase = await readDataBase();
+//   const updatedDataBase = dataBase.filter(
+//     (contact) => contact.id !== contactId
+//   );
+//   await writeDataBase(updatedDataBase);
+// };
 
-const createContact = async (name, email, phone) => {
-  const id = uuidv4();
-  const contact = { id, name, email, phone };
-  const dataBase = await readDataBase();
-  dataBase.push(contact);
+// const createContact = async (name, email, phone) => {
+//   const id = uuidv4();
+//   const contact = { id, name, email, phone };
+//   const dataBase = await readDataBase();
+//   dataBase.push(contact);
 
-  await writeDataBase(dataBase);
+//   await writeDataBase(dataBase);
 
-  return contact;
-};
+//   return contact;
+// };
 
-const updateContact = async (contactId, body) => {
-  try {
-    const dataBase = await readDataBase();
-    const index = dataBase.findIndex((contact) => contact.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    dataBase[index] = { id: contactId, ...body };
-    await writeDataBase(dataBase);
-    return dataBase[index];
-  } catch (err) {
-    return console.log(err.message);
+// const updateContact = async (contactId, body) => {
+//   try {
+//     const dataBase = await readDataBase();
+//     const index = dataBase.findIndex((contact) => contact.id === contactId);
+//     if (index === -1) {
+//       return null;
+//     }
+//     dataBase[index] = { id: contactId, ...body };
+//     await writeDataBase(dataBase);
+//     return dataBase[index];
+//   } catch (err) {
+//     return console.log(err.message);
+//   }
+// };
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   deleteContact,
+//   createContact,
+//   updateContact,
+// };
+
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
   }
-};
+);
+
+const Contact = mongoose.model("Contacts", schema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  deleteContact,
-  createContact,
-  updateContact,
+  Contact,
 };
